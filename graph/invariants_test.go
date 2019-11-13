b796393380d5f85efe3ccf3bcfd9a88a42ccbf64
Numeri <Numeri@users.noreply.github.com>
2019-11-13 04:29:43 -0500

    Added tests

--- graph/invariants_test.go
+++ graph/invariants_test.go
@@ -46,3 +46,61 @@ func TestDegreeSequence(t *testing.T) {
 		}
 	}
 }
+
+func TestAdjecentVertices(t *testing.T) {
+	edges := []graph.Edge{
+		{0, 1},
+		{0, 2},
+		{0, 3},
+		{1, 3},
+		{1, 2},
+	}
+
+	g := graph.NewGraph(5)
+	g = g.AddEdges(edges)
+
+	received := g.AdjecentVertices(0)
+	expected := []graph.Vertex{1, 2, 3}
+
+	if len(received) != len(expected) {
+		t.Errorf("DegreeSequence: expected %v, received %v", expected, received)
+	}
+
+	for i := range received {
+		if received[i] != expected[i] {
+			t.Errorf("DegreeSequence: expected %v, received %v", expected, received)
+		}
+	}
+}
+
+func TestVerticesConnected(t *testing.T) {
+	edges := []graph.Edge{
+		{0, 1},
+		{0, 2},
+		{0, 3},
+		{1, 3},
+		{1, 2},
+	}
+
+	g := graph.NewGraph(5)
+	g = g.AddEdges(edges)
+
+	input := [][]graph.Vertex{
+		{0, 1},
+		{0, 4},
+		{2, 3},
+		{4, 4},
+	}
+	expected := []bool{true, false, true, false}
+	received := make([]bool, len(expected))
+
+	for i := range received {
+		received[i] = g.VerticesConnected(input[i][0], input[i][1])
+	}
+
+	for i := range received {
+		if received[i] != expected[i] {
+			t.Errorf("VerticesConnected: for the vertices %v and %v, expected %v, received %v", input[i][0], input[i][1], expected[i], received[i])
+		}
+	}
+}
