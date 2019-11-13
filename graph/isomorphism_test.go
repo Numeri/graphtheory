735a2ec201f3a87fae00d1415c2ac12cb3377783
Numeri <Numeri@users.noreply.github.com>
2019-11-13 04:19:16 -0500

    Accidently switched files

--- graph/isomorphism_test.go
+++ graph/isomorphism_test.go
@@ -5,44 +5,7 @@ import (
 	"testing"
 )
 
-func TestNumEdges(t *testing.T) {
-	edges := []graph.Edge{
-		{0, 1},
-		{0, 2},
-		{0, 3},
-		{1, 3},
-		{1, 2},
-	}
-
-	g := graph.NewGraph(4)
-	g = g.AddEdges(edges)
-
-	received := g.NumEdges()
-	expected := uint(len(edges))
-
-	if received != expected {
-		t.Errorf("NumEdges: expected %d, received %d", expected, received)
-	}
-}
-
-func TestDegreeSequence(t *testing.T) {
-	edges := []graph.Edge{
-		{0, 1},
-		{0, 2},
-		{0, 3},
-		{1, 3},
-		{1, 2},
-	}
-
-	g := graph.NewGraph(5)
-	g = g.AddEdges(edges)
-
-	received := g.DegreeSequence()
-	expected := []uint{3, 3, 2, 2, 0}
-
-	for i := range received {
-		if received[i] != expected[i] {
-			t.Errorf("DegreeSequence: expected %v, received %v", expected, received)
-		}
-	}
+func BenchmarkFilterNGraphIsos(b *testing.B) {
+	n := uint8(6)
+	graph.FilterNGraphIsos(n)
 }
